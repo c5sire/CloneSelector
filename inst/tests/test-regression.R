eca77697376c8774a37eb335cd747de19b836086
mostly-harmless <mostly.harmless.71@gmail.com>
2014-01-18 18:27:09 -0800

    update

--- inst/tests/test-regression.R
+++ inst/tests/test-regression.R
@@ -23,19 +23,20 @@ source("tools/analysis/regression.R")
 
 result <- regression("diamonds", "price", c("carat", "clarity"), NULL, NULL, "none", FALSE, FALSE, FALSE, "scatterlist")
 
-sink("../tests/regression_no_runApp.txt")
-  summary_regression(result)
-sink()
+# sink("../tests/regression_no_runApp.txt")
+#   summary_regression(result)
+# sink()
+
+# works!
+res1 <- capture.output(summary_regression(result))
+res2 <- paste0(readLines("../tests/regression_correct.txt"))
+all.equal(res1,res2)
 
 png('../tests/regression_no_runApp.png')
   plots_regression(result)
 dev.off()
 
 # works!
-res1 <- paste0(readLines("../tests/regression_no_runApp.txt"), collapse = "\n")
-res2 <- paste0(readLines("../tests/regression_correct.txt"), collapse = "\n")
-all.equal(res1,res2)
-
 res1 <- readPNG("../tests/regression_no_runApp.png")
 res2 <- readPNG("../tests/regression_correct.png")
 all.equal(res1,res2)
@@ -152,6 +153,7 @@ values
 # calling reactives directly doesn't work
 # with full ui.R from marketing app
 ######################################
+# source("global.R", local = TRUE)
 source("global.R", local = TRUE)
 
 # copy-and-paste from inst/marketing/ui.R
@@ -243,6 +245,9 @@ server <- function(session, input, output) {
       summary_regression(result)
     sink()
 
+    # updateTabsetPanel(session, "nav_radiant", selected = "Report") 
+
+
     if(!is.character(result))
       stopApp()
   })
@@ -256,4 +261,84 @@ all.equal(res1,res2)
 
 res1
 
+
+
+######################################
+# calling reactives directly doesn't work
+# with full ui.R from marketing app
+######################################
+# source("global.R", local = TRUE)
+# source("global.R")
+
+# copy-and-paste from inst/marketing/ui.R
+ui <- navbarPage("Radiant", id = "nav_radiant", inverse = TRUE, collapsable = TRUE,
+
+  tabPanel("Data", uiOutput('data_ui_and_tabs')),
+
+  navbarMenu("Random",
+    tabPanel("Sampling and assignment", uiOutput("random")),
+    tabPanel("Sample size", uiOutput("sampleSize"))
+  ),
+
+  navbarMenu("Base",
+    tabPanel("Single mean", uiOutput("singleMean")),
+    tabPanel("Compare means", uiOutput("compareMeans")),
+    tabPanel("Cross-tabs", uiOutput("crosstab"))
+  ),
+
+  navbarMenu("Regression",
+    tabPanel("Correlation", uiOutput("correlation")),
+    tabPanel("Linear (OLS)", uiOutput("regression"))
+  ),
+
+  navbarMenu("Maps",
+    tabPanel("(Dis)similarity", uiOutput("mds")),
+    tabPanel("Attributes", uiOutput("pmap"))
+  ),
+
+  navbarMenu("Factor",
+    tabPanel("Pre-factor analysis", uiOutput("preFactor")),
+    tabPanel("Factor analysis", uiOutput("fullFactor"))
+  ),
+
+  navbarMenu("Cluster",
+    tabPanel("Hierarchical", uiOutput("hierCluster")),
+    tabPanel("K-means", uiOutput("kmeansCluster"))
+  ),
+
+  navbarMenu("Conjoint",
+    tabPanel("Create profiles", uiOutput("conjointProfiles")),
+    tabPanel("Conjoint analysis", uiOutput("conjoint"))
+  ),
+
+  navbarMenu("R",
+    tabPanel("Report", uiOutput("report")),
+    tabPanel("Code", uiOutput("rcode"))
+  ),
+
+  tabPanel("State", uiOutput("state")),
+
+  tabPanel("About", includeRmd("tools/app/about.Rmd"))
+
+
+)
+
+server <- function(input, output, session) {
+
+  # source("global.R", local = TRUE)
+  source("global.R")
+  # state_list$datasets <<- "diamonds"
+  # state_list$reg_var1 <<- "price"
+  # state_list$reg_var2 <<- c("carat","clarity")
+  # state_list$reg_standardize <<- FALSE
+
+  source('radiant.R', local = TRUE)
+  R.utils::sourceDirectory('tools/analysis', recursive = TRUE)
+  R.utils::sourceDirectory('tools/data', recursive = TRUE)
+  R.utils::sourceDirectory('tools/app', recursive = TRUE)
+
+}
+
+# app doesn't work when launched using list() in runApp
+runApp(list(ui = ui, server = server))
 
