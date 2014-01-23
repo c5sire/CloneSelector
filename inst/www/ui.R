39c095114cd69e8695b32a6f01f56031d3901373
Reinhard Simon <R.SIMON@CGIAR.ORG>
2014-01-23 18:47:33 -0500

    Reorganizing for migration

---
+++ inst/www/ui.R
@@ -0,0 +1,51 @@
+shinyUI(navbarPage("CloneSelector", id = "nav_radiant", inverse = TRUE, collapsable = TRUE,
+
+    tabPanel("Design", uiOutput('data')),                   
+                   
+    tabPanel("Data", uiOutput('data_ui_and_tabs')),
+# 
+#   navbarMenu("Random",
+#     tabPanel("Sampling and assignment", uiOutput("random")),
+#     tabPanel("Sample size", uiOutput("sampleSize"))
+#   ),
+# 
+#   navbarMenu("Base",
+#     tabPanel("Single mean", uiOutput("singleMean")),
+#     tabPanel("Compare means", uiOutput("compareMeans")),
+#     tabPanel("Cross-tabs", uiOutput("crosstab"))
+#   ),
+# 
+#   navbarMenu("Regression",
+#     tabPanel("Correlation", uiOutput("correlation")),
+#     tabPanel("Linear (OLS)", uiOutput("regression"))
+#   ),
+# 
+#   navbarMenu("Maps",
+#     tabPanel("(Dis)similarity", uiOutput("mds")),
+#     tabPanel("Attributes", uiOutput("pmap"))
+#   ),
+# 
+#   navbarMenu("Factor",
+#     tabPanel("Pre-factor analysis", uiOutput("preFactor")),
+#     tabPanel("Factor analysis", uiOutput("fullFactor"))
+#   ),
+# 
+#   navbarMenu("Cluster",
+#     tabPanel("Hierarchical", uiOutput("hierCluster")),
+#     tabPanel("K-means", uiOutput("kmeansCluster"))
+#   ),
+# 
+#   navbarMenu("Conjoint",
+#     tabPanel("Create profiles", uiOutput("conjointProfiles")),
+#     tabPanel("Conjoint analysis", uiOutput("conjoint"))
+#   ),
+
+  navbarMenu("Tools",
+    tabPanel("Report", uiOutput("report"))#,
+    #tabPanel("Code", uiOutput("rcode"))
+  ),
+
+  tabPanel("State", uiOutput("state")),
+
+  tabPanel("About", includeRmd("tools/app/about.Rmd"))
+))
